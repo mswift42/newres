68394b48df62201fe68eb58abc0ba514350d03a0
mswift42 <martin.haesler@gmail.com>
2016-07-31 14:03:07 +0100

    Fix layout__header-transparent

--- webapp/src/cljs/webapp/components.cljs
+++ webapp/src/cljs/webapp/components.cljs
@@ -3,7 +3,7 @@
 
 (defn navbar-component [content]
   [:div.layoutheader-transparent.mdl-layout.mdl-js-layout
-   [:header.mdl-layout__header.mdl-layout__header__transparent
+   [:header.mdl-layout__header.mdl-layout__header--transparent
     [:div.mdl-layout__header-row
      [:span.mdl-layout-title "Portfolio"]
      [:div.mdl-layout-spacer]
@@ -16,4 +16,6 @@
      [:a.mdl-navigation__link {:href ""} "About"]
      [:a.mdl-navigation__link {:href ""} "Projects"]]]
    [:main.mdl-layout__content
-    content]])
+    [:div.content
+     content]]])
+
