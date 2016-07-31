1c238ac453017e595535b2070c8e596d965bc694
mswift42 <martin.haesler@gmail.com>
2016-07-31 11:29:39 +0100

    Add navbar-component

---
+++ webapp/src/cljs/webapp/components.cljs
@@ -0,0 +1,19 @@
+(ns webapp.components)
+
+
+(defn navbar-component [content]
+  [:div.layoutheader-transparent.mdl-layout.mdl-js-layout
+   [:header.mdl-layout__header.mdl-layout__header__transparent
+    [:div.mdl-layout__header-row
+     [:span.mdl-layout-title "Portfolio"]
+     [:div.mdl-layout-spacer]
+     [:nav.mdl-navigation
+      [:a.mdl-navigation__link {:href ""} "About"]
+      [:a.mdl-navigation__link {:href ""} "Projects"]]]]
+   [:div.mdl-layout__drawer
+    [:span.mdl-layout-title "Portfolio"]
+    [:nav.mdl-navigation
+     [:a.mdl-navigation__link {:href ""} "About"]
+     [:a.mdl-navigation__link {:href ""} "Projects"]]]
+   [:main.mdl-layout__content
+    content]])
